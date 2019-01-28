4180a0b67b963413c0f610b48002f59b81c9fe70
fastsystem <info@fast-system.jp>
2019-01-29 07:17:54 +0900

    fix gui source

--- Assets/CameraFocus.cs
+++ Assets/CameraFocus.cs
@@ -3,58 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFocus : MonoBehaviour {
-
-    public Camera MainCamera;
-    public GameObject White;
-    public GameObject Red;
-    public GameObject Green;
-    public GameObject Blue;
-
+public class CameraFocus : MonoBehaviour
+{
     public float moveTime = 0.5f;
 
     public float defautDistance = 5.0f;
 
-    private bool val0 = true;
-    private bool val1 = true;
-    private bool val2 = false;
-    private bool val3 = false;
-
     private Vector3? start = null;
     private Vector3? end = null;
+    private Camera movingCamera = null;
     private float movingTime = 0.0f;
 
 	void Update () {
         if (start == null) return;
 
         movingTime += Time.deltaTime;
-        MainCamera.transform.position = Vector3.Slerp(start.Value, end.Value, (movingTime / moveTime));
+        movingCamera.transform.position = Vector3.Slerp(start.Value, end.Value, (movingTime / moveTime));
 
         if (moveTime < movingTime)
         {
-            MainCamera.transform.position = end.Value;
+            movingCamera.transform.position = end.Value;
             start = null;
             end = null;
-        }
-    }
-
-    private void OnGUI()
-    {
-        val0 = GUI.Toggle(new Rect(Screen.width - 100, 10, 90, 20), val0, "White");
-        val1 = GUI.Toggle(new Rect(Screen.width - 100, 30, 90, 20), val1, "Red");
-        val2 = GUI.Toggle(new Rect(Screen.width - 100, 50, 90, 20), val2, "Blue");
-        val3 = GUI.Toggle(new Rect(Screen.width - 100, 70, 90, 20), val3, "Green");
-        var btn = GUI.Button(new Rect(Screen.width - 100, 90, 90, 20), "Focus");
-        if (btn)
-        {
-            var targets = new List<GameObject>();
-            if (val0) targets.Add(White);
-            if (val1) targets.Add(Red);
-            if (val2) targets.Add(Blue);
-            if (val3) targets.Add(Green);
-            if (targets.Count == 0) return;
-
-            this.Focus(MainCamera, targets);
+            movingCamera = null;
         }
     }
 
@@ -111,5 +82,6 @@ public class CameraFocus : MonoBehaviour {
         this.start = camera.transform.position;
         this.end = targetCenter - (relative - camera.transform.position);
         this.movingTime = 0.0f;
+        this.movingCamera = camera;
     }
 }
---
+++ Assets/CameraFocusGui.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocusGui : MonoBehaviour
+{
+    public Camera MainCamera;
+    public GameObject White;
+    public GameObject Red;
+    public GameObject Green;
+    public GameObject Blue;
+
+    private bool val0 = true;
+    private bool val1 = true;
+    private bool val2 = false;
+    private bool val3 = false;
+
+    private void OnGUI()
+    {
+        val0 = GUI.Toggle(new Rect(Screen.width - 100, 10, 90, 20), val0, "White");
+        val1 = GUI.Toggle(new Rect(Screen.width - 100, 30, 90, 20), val1, "Red");
+        val2 = GUI.Toggle(new Rect(Screen.width - 100, 50, 90, 20), val2, "Blue");
+        val3 = GUI.Toggle(new Rect(Screen.width - 100, 70, 90, 20), val3, "Green");
+        var btn = GUI.Button(new Rect(Screen.width - 100, 90, 90, 20), "Focus");
+        if (btn)
+        {
+            var targets = new List<GameObject>();
+            if (val0) targets.Add(White);
+            if (val1) targets.Add(Red);
+            if (val2) targets.Add(Blue);
+            if (val3) targets.Add(Green);
+            if (targets.Count == 0) return;
+
+            this.GetComponent<CameraFocus>().Focus(MainCamera, targets);
+        }
+    }
+}
